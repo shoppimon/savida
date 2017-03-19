f4c26eb2c3cf9abe4dea4914518b3adb8bd317f4
Vladimir Zeifman <vladimir@shoppimon.com>
2017-03-19 18:55:26 +0200

    renamed interceptor to WSGIApplication
    - implemented `Interceptor` class that is responsible for intercepting http responses

--- imitator.py
+++ imitator.py
@@ -5,17 +5,13 @@ from werkzeug.serving import run_simple
 from werkzeug.wsgi import SharedDataMiddleware
 from werkzeug.routing import Map, Rule
 from werkzeug.exceptions import HTTPException, NotFound
+import time
 
 
-class Interceptor(object):
+class WSGIApplication(object):
     def __init__(self, app):
-        self.url_map = Map([
-            Rule('/imitator.py', endpoint=self.intercept)
-        ])
+        self.url_map = Map([])
         self.app = app
-
-    def intercept(self, request):
-        return Response(response='response was intercepted', status=200)
 
     def __call__(self, environ, start_response):
         # try to match URL from url_map
@@ -38,6 +34,58 @@ static_files_middleware = SharedDataMiddleware(not_found_middleware, {
     '/': os.path.dirname(__file__)
 })
 
-application = Interceptor(static_files_middleware)
+wsgi_application = WSGIApplication(static_files_middleware)
 
-run_simple('127.0.0.1', 5000, application, use_debugger=True, use_reloader=True, threaded=True)
+
+class Interceptor(object):
+    def __init__(self):
+        self.paths = []
+        self.rules = []
+
+    def when(self, path):
+        self.paths.append(
+            path
+        )
+
+        return self
+
+    def call(self, callback):
+        path = self.paths.pop()
+
+        def callback_wrapper(request):
+            response = callback(request)
+
+            # pass request to static middleware by raising NotFound
+            if not response:
+                raise NotFound()
+
+            # otherwise, return the response
+            return response
+
+        self.rules.append(Rule(path, endpoint=callback_wrapper))
+
+    def response(self, *args, **kwargs):
+        # server_instance.when('/some/path').response(status=404, response='im 404')
+        # pass same parameters as Response() expects
+        # we should return Response() object
+        def response_wrapper(request):
+            return Response(*args, **kwargs)
+
+        path = self.paths.pop()
+        self.rules.append(Rule(path, endpoint=response_wrapper))
+
+    def start(self):
+        # plug the rules
+        wsgi_application.url_map = Map(self.rules)
+        run_simple('127.0.0.1', 5000, wsgi_application, use_debugger=True, use_reloader=True, threaded=True)
+
+    def wait(self, seconds):
+        # server_instance.when('/some/path').wait(seconds=5)
+        def callback(request):
+            time.sleep(seconds)
+            # raise exception so that the static middleware will handle this request
+            raise NotFound()
+
+        path = self.paths.pop()
+        self.rules.append(Rule(path, endpoint=callback))
+
