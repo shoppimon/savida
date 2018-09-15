93b7972af35bfd6e9fb5656e55cd0d1a80ed807b
Shahar Evron <shahar@shoppimon.com>
2018-09-15 18:32:40 +0300

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,14 @@ from setuptools import setup, find_packages
 setup(
     name='savida',
     version=open('VERSION').read(),
-    description='Savida - Testing HTTP Server',
+    description='Savida - Testing HTTP Server Fixture',
+    keywords='http server testing fixture',
     author='Shahar Evron',
     author_email='shahar@shoppimon.com',
     license='Apache 2.0',
     url='https://github.com/shoppimon/savida',
     packages=find_packages(),
     install_requires=['Werkzeug'],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 )
